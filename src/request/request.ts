e4148d1c10741e9797b491304247620903873e43
Rostislav <spellmane21@gmail.com>
2022-06-06 18:48:20 +0200

    main / fix api

--- src/request/request.ts
+++ src/request/request.ts
@@ -1,14 +1,9 @@
 import axios from "axios";
 import { IGraphRequest } from "./interfaces";
 
-const API =
-  process.env.NODE_ENV === "production"
-    ? "https://yourapp.herokuapp.com/api"
-    : process.env.REACT_APP_API_URL;
-
 export const graphRequest = async ({ query, name }: IGraphRequest) => {
   const response = await axios.post(
-    `${API}/graphql`,
+    `https://yourapp.herokuapp.com/api/graphql`,
     { query },
     {
       headers: {
